37d1313373d7b9cd6e2afc1b722497edcbc5bac1
ultra-hip <michael.kahle@yahoo.de>
2016-10-13 16:43:44 +0200

    Ant build file added, some restructuring

--- proxy/certs/create.sh
+++ proxy/certs/create.sh
@@ -1,1 +1,1 @@
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout www.example.com/domain.key -out www.example.com/domain.crt
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout domain.key -out domain.crt
