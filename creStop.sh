00d18c2e34b6b8df2cfdedf8a59fada89ca3240a
Michael Kahle <michael.kahle@yahoo.de>
2020-04-01 12:11:49 +0200

    Start and stop scripts

---
+++ creStop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Show status
+echo "$(docker ps -a)"
+echo "$(ls -l /data/dockerCRE/volumes/postgres/dumps/*/*)"
+
+#Stop containers
+echo "Stop CRE containers"
+docker stop creglue creproxy crenginxphp2 crephp2a crepostgis crenginxphp crephp cremysql
+docker rm creglue creproxy crenginxphp2 crephp2a crepostgis crenginxphp crephp cremysql
+
+
